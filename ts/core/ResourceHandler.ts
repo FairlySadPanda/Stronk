bd10ee56ff4433f0649a83d69a40d125ca2759ed
Huw Dawson <karakkaraz@gmail.com>
2019-08-20 14:43:43 +0100

    ResourceHandlear's properties moved into the class def

--- ts/core/ResourceHandler.ts
+++ ts/core/ResourceHandler.ts
@@ -2,63 +2,52 @@ import SceneManager from "../managers/SceneManager";
 import Graphics from "./Graphics";
 
 export default abstract class ResourceHandler {
-    public static _reloaders: any[];
-    public static _defaultRetryInterval: number[];
-    public static createLoader: (
-        url: any,
-        retryMethod: any,
-        resignMethod?: any,
-        retryInterval?: any
-    ) => () => void;
-    public static exists: () => boolean;
-    public static retry: () => void;
-}
+    private static _reloaders = [];
+    private static _defaultRetryInterval = [500, 1000, 3000];
 
-ResourceHandler._reloaders = [];
-ResourceHandler._defaultRetryInterval = [500, 1000, 3000];
-
-ResourceHandler.createLoader = function(
-    url,
-    retryMethod,
-    resignMethod?,
-    retryInterval?
-): () => void {
-    retryInterval = retryInterval || this._defaultRetryInterval;
-    const reloaders = this._reloaders;
-    let retryCount = 0;
-    return function() {
-        if (retryCount < retryInterval.length) {
-            setTimeout(retryMethod, retryInterval[retryCount]);
-            retryCount++;
-        } else {
-            if (resignMethod) {
-                resignMethod();
-            }
-            if (url) {
-                if (reloaders.length === 0) {
-                    Graphics.printLoadingError(url);
-                    SceneManager.stop();
+    public static createLoader(
+        url: string,
+        retryMethod: (any?: any) => any,
+        resignMethod?: (any?: any) => any,
+        retryInterval?: number[]
+    ): () => void {
+        retryInterval = retryInterval || this._defaultRetryInterval;
+        const reloaders = this._reloaders;
+        let retryCount = 0;
+        return function() {
+            if (retryCount < retryInterval.length) {
+                setTimeout(retryMethod, retryInterval[retryCount]);
+                retryCount++;
+            } else {
+                if (resignMethod) {
+                    resignMethod();
                 }
-                reloaders.push(function() {
-                    retryCount = 0;
-                    retryMethod();
-                });
+                if (url) {
+                    if (reloaders.length === 0) {
+                        Graphics.printLoadingError(url);
+                        SceneManager.stop();
+                    }
+                    reloaders.push(function() {
+                        retryCount = 0;
+                        retryMethod();
+                    });
+                }
             }
-        }
-    };
-};
-
-ResourceHandler.exists = function() {
-    return this._reloaders.length > 0;
-};
-
-ResourceHandler.retry = function() {
-    if (this._reloaders.length > 0) {
-        Graphics.eraseLoadingError();
-        SceneManager.resume();
-        this._reloaders.forEach(function(reloader) {
-            reloader();
-        });
-        this._reloaders.length = 0;
+        };
     }
-};
+
+    public static exists() {
+        return this._reloaders.length > 0;
+    }
+
+    public static retry() {
+        if (this._reloaders.length > 0) {
+            Graphics.eraseLoadingError();
+            SceneManager.resume();
+            this._reloaders.forEach(function(reloader) {
+                reloader();
+            });
+            this._reloaders.length = 0;
+        }
+    }
+}
