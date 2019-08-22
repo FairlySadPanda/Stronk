6b7eba9343fab4285fb2a606c554fb16ff4e7d11
Huw Dawson <karakkaraz@gmail.com>
2019-08-22 23:11:24 +0100

    SceneManager static this cleanup

--- ts/managers/SceneManager.ts
+++ ts/managers/SceneManager.ts
@@ -7,16 +7,13 @@ import WebAudio from "../core/WebAudio";
 import AudioManager from "./AudioManager";
 import ImageManager from "./ImageManager";
 import PluginManager from "./PluginManager";
+import Scene_Base from "../scenes/Scene_Base";
 
 declare const nw: any;
 
 export default abstract class SceneManager {
-    private static _getTimeInMsWithoutMobileSafari = function() {
-        return performance.now();
-    };
-
-    private static _scene = null;
-    private static _nextScene = null;
+    private static _scene: Scene_Base = null;
+    private static _nextScene: Scene_Base = null;
     private static _stack = [];
     private static _stopped = false;
     private static _sceneStarted = false;
@@ -29,7 +26,7 @@ export default abstract class SceneManager {
     private static _boxHeight = 624;
     private static _deltaTime = 1.0 / 60.0;
     private static _accumulator = 0.0;
-    private static _currentTime = SceneManager._getTimeInMsWithoutMobileSafari();
+    private static _currentTime = performance.now();
 
     /*
      * Gets the current time in ms without on iOS Safari.
@@ -38,32 +35,36 @@ export default abstract class SceneManager {
 
     public static run = function(sceneClass: any) {
         try {
-            this.initialize();
-            this.goto(sceneClass);
-            this.requestUpdate();
+            SceneManager.initialize();
+            SceneManager.goto(sceneClass);
+            SceneManager.requestUpdate();
         } catch (e) {
-            this.catchException(e);
+            SceneManager.catchException(e);
         }
     };
 
     public static initialize = function() {
-        this.initGraphics();
-        this.checkFileAccess();
-        this.initAudio();
-        this.initInput();
-        this.initNwjs();
-        this.checkPluginErrors();
-        this.setupErrorHandlers();
+        SceneManager.initGraphics();
+        SceneManager.checkFileAccess();
+        SceneManager.initAudio();
+        SceneManager.initInput();
+        SceneManager.initNwjs();
+        SceneManager.checkPluginErrors();
+        SceneManager.setupErrorHandlers();
     };
 
     public static initGraphics = function() {
-        const type = this.preferableRendererType();
-        Graphics.initialize(this._screenWidth, this._screenHeight, type);
-        Graphics.boxWidth = this._boxWidth;
-        Graphics.boxHeight = this._boxHeight;
+        const type = SceneManager.preferableRendererType();
+        Graphics.initialize(
+            SceneManager._screenWidth,
+            SceneManager._screenHeight,
+            type
+        );
+        Graphics.boxWidth = SceneManager._boxWidth;
+        Graphics.boxHeight = SceneManager._boxHeight;
         Graphics.setLoadingImage("img/system/Loading.png");
         if (type === "webgl") {
-            this.checkWebGL();
+            SceneManager.checkWebGL();
         }
     };
 
@@ -122,25 +123,31 @@ export default abstract class SceneManager {
     };
 
     public static setupErrorHandlers = function() {
-        window.addEventListener("error", this.onError.bind(this));
-        document.addEventListener("keydown", this.onKeyDown.bind(this));
+        window.addEventListener(
+            "error",
+            SceneManager.onError.bind(SceneManager)
+        );
+        document.addEventListener(
+            "keydown",
+            SceneManager.onKeyDown.bind(SceneManager)
+        );
     };
 
     public static requestUpdate = function() {
-        if (!this._stopped) {
-            requestAnimationFrame(this.update.bind(this));
+        if (!SceneManager._stopped) {
+            requestAnimationFrame(SceneManager.update.bind(SceneManager));
         }
     };
 
     public static update = function() {
         try {
             if (Utils.isMobileSafari()) {
-                this.updateInputData();
+                SceneManager.updateInputData();
             }
-            this.updateManagers();
-            this.updateMain();
+            SceneManager.updateManagers();
+            SceneManager.updateMain();
         } catch (e) {
-            this.catchException(e);
+            SceneManager.catchException(e);
         }
     };
 
@@ -156,7 +163,7 @@ export default abstract class SceneManager {
         console.error(e.message);
         console.error(e.filename, e.lineno);
         try {
-            this.stop();
+            SceneManager.stop();
             Graphics.printError("Error", e.message);
             AudioManager.stopAll();
         } catch (e2) {}
@@ -191,7 +198,7 @@ export default abstract class SceneManager {
             Graphics.printError("UnknownError", e);
         }
         AudioManager.stopAll();
-        this.stop();
+        SceneManager.stop();
     };
 
     public static updateInputData = function() {
@@ -201,25 +208,25 @@ export default abstract class SceneManager {
 
     public static updateMain = function() {
         if (Utils.isMobileSafari()) {
-            this.changeScene();
-            this.updateScene();
+            SceneManager.changeScene();
+            SceneManager.updateScene();
         } else {
-            const newTime = this._getTimeInMsWithoutMobileSafari();
-            let fTime = (newTime - this._currentTime) / 1000;
+            const newTime = performance.now();
+            let fTime = (newTime - SceneManager._currentTime) / 1000;
             if (fTime > 0.25) {
                 fTime = 0.25;
             }
-            this._currentTime = newTime;
-            this._accumulator += fTime;
-            while (this._accumulator >= this._deltaTime) {
-                this.updateInputData();
-                this.changeScene();
-                this.updateScene();
-                this._accumulator -= this._deltaTime;
+            SceneManager._currentTime = newTime;
+            SceneManager._accumulator += fTime;
+            while (SceneManager._accumulator >= SceneManager._deltaTime) {
+                SceneManager.updateInputData();
+                SceneManager.changeScene();
+                SceneManager.updateScene();
+                SceneManager._accumulator -= SceneManager._deltaTime;
             }
         }
-        this.renderScene();
-        this.requestUpdate();
+        SceneManager.renderScene();
+        SceneManager.requestUpdate();
     };
 
     public static updateManagers = function() {
@@ -227,44 +234,47 @@ export default abstract class SceneManager {
     };
 
     public static changeScene = function() {
-        if (this.isSceneChanging() && !this.isCurrentSceneBusy()) {
-            if (this._scene) {
-                this._scene.terminate();
-                this._scene.detachReservation();
-                this._previousClass = this._scene.constructor;
+        if (
+            SceneManager.isSceneChanging() &&
+            !SceneManager.isCurrentSceneBusy()
+        ) {
+            if (SceneManager._scene) {
+                SceneManager._scene.terminate();
+                SceneManager._scene.detachReservation();
+                SceneManager._previousClass = SceneManager._scene.constructor;
             }
-            this._scene = this._nextScene;
-            if (this._scene) {
-                this._scene.attachReservation();
-                this._scene.create();
-                this._nextScene = null;
-                this._sceneStarted = false;
-                this.onSceneCreate();
+            SceneManager._scene = SceneManager._nextScene;
+            if (SceneManager._scene) {
+                SceneManager._scene.attachReservation();
+                SceneManager._scene.create();
+                SceneManager._nextScene = null;
+                SceneManager._sceneStarted = false;
+                SceneManager.onSceneCreate();
             }
-            if (this._exiting) {
-                this.terminate();
+            if (SceneManager._exiting) {
+                SceneManager.terminate();
             }
         }
     };
 
     public static updateScene = function() {
-        if (this._scene) {
-            if (!this._sceneStarted && this._scene.isReady()) {
-                this._scene.start();
-                this._sceneStarted = true;
-                this.onSceneStart();
+        if (SceneManager._scene) {
+            if (!SceneManager._sceneStarted && SceneManager._scene.isReady()) {
+                SceneManager._scene.start();
+                SceneManager._sceneStarted = true;
+                SceneManager.onSceneStart();
             }
-            if (this.isCurrentSceneStarted()) {
-                this._scene.update();
+            if (SceneManager.isCurrentSceneStarted()) {
+                SceneManager._scene.update();
             }
         }
     };
 
     public static renderScene = function() {
-        if (this.isCurrentSceneStarted()) {
-            Graphics.render(this._scene);
-        } else if (this._scene) {
-            this.onSceneLoading();
+        if (SceneManager.isCurrentSceneStarted()) {
+            Graphics.render(SceneManager._scene);
+        } else if (SceneManager._scene) {
+            SceneManager.onSceneLoading();
         }
     };
 
@@ -281,83 +291,86 @@ export default abstract class SceneManager {
     };
 
     public static isSceneChanging = function() {
-        return this._exiting || !!this._nextScene;
+        return SceneManager._exiting || !!SceneManager._nextScene;
     };
 
     public static isCurrentSceneBusy = function() {
-        return this._scene && this._scene.isBusy();
+        return SceneManager._scene && SceneManager._scene.isBusy();
     };
 
     public static isCurrentSceneStarted = function() {
-        return this._scene && this._sceneStarted;
+        return SceneManager._scene && SceneManager._sceneStarted;
     };
 
     public static isNextScene = function(sceneClass: any) {
-        return this._nextScene && this._nextScene.constructor === sceneClass;
+        return (
+            SceneManager._nextScene &&
+            SceneManager._nextScene.constructor === sceneClass
+        );
     };
 
     public static isPreviousScene = function(sceneClass: any) {
-        return this._previousClass === sceneClass;
+        return SceneManager._previousClass === sceneClass;
     };
 
-    public static goto = function(SceneClass: new () => void) {
+    public static goto = function(SceneClass: new () => Scene_Base) {
         if (SceneClass) {
-            this._nextScene = new SceneClass();
+            SceneManager._nextScene = new SceneClass();
         }
-        if (this._scene) {
-            this._scene.stop();
+        if (SceneManager._scene) {
+            SceneManager._scene.stop();
         }
     };
 
     public static push = function(sceneClass: any) {
-        this._stack.push(this._scene.constructor);
-        this.goto(sceneClass);
+        SceneManager._stack.push(SceneManager._scene.constructor);
+        SceneManager.goto(sceneClass);
     };
 
     public static pop = function() {
-        if (this._stack.length > 0) {
-            this.goto(this._stack.pop());
+        if (SceneManager._stack.length > 0) {
+            SceneManager.goto(SceneManager._stack.pop());
         } else {
-            this.exit();
+            SceneManager.exit();
         }
     };
 
     public static exit = function() {
-        this.goto(null);
-        this._exiting = true;
+        SceneManager.goto(null);
+        SceneManager._exiting = true;
     };
 
     public static clearStack = function() {
-        this._stack = [];
+        SceneManager._stack = [];
     };
 
     public static stop = function() {
-        this._stopped = true;
+        SceneManager._stopped = true;
     };
 
     public static prepareNextScene = function(...args: any) {
-        this._nextScene.prepare.apply(this._nextScene, args);
+        SceneManager._nextScene.prepare.apply(SceneManager._nextScene, args);
     };
 
     public static snap = function() {
-        return Bitmap.snap(this._scene);
+        return Bitmap.snap(SceneManager._scene);
     };
 
     public static snapForBackground = function() {
-        this._backgroundBitmap = this.snap();
-        this._backgroundBitmap.blur();
+        SceneManager._backgroundBitmap = SceneManager.snap();
+        SceneManager._backgroundBitmap.blur();
     };
 
     public static backgroundBitmap = function() {
-        return this._backgroundBitmap;
+        return SceneManager._backgroundBitmap;
     };
 
     public static resume = function() {
-        this._stopped = false;
-        this.requestUpdate();
+        SceneManager._stopped = false;
+        SceneManager.requestUpdate();
         if (!Utils.isMobileSafari()) {
-            this._currentTime = this._getTimeInMsWithoutMobileSafari();
-            this._accumulator = 0;
+            SceneManager._currentTime = performance.now();
+            SceneManager._accumulator = 0;
         }
     };
 }
