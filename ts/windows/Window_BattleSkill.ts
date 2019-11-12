5f58806cee62e0be6620db061a077327034396ca
Huw FSP Dawson <karakkaraz@gmail.com>
2019-11-12 00:38:08 +0000

    BattkeSkill tracks the active player (better UI)

--- ts/windows/Window_BattleSkill.ts
+++ ts/windows/Window_BattleSkill.ts
@@ -1,4 +1,6 @@
 import { Window_SkillList } from "./Window_SkillList";
+import { Config } from "../Config";
+import { ConfigManager } from "../managers/ConfigManager";
 
 // -----------------------------------------------------------------------------
 // Window_BattleSkill
@@ -6,12 +8,19 @@ import { Window_SkillList } from "./Window_SkillList";
 // The window for selecting a skill to use on the battle screen.
 
 export class Window_BattleSkill extends Window_SkillList {
-    public constructor(x, y, width, height) {
+    public constructor(x: number, y: number, width: number, height: number) {
         super(x, y, width, height);
         this.hide();
     }
 
+    public maxCols() {
+        return 1;
+    }
+
     public show() {
+        const sprite = $gameActors.actor(this._actor.actorId()).battler();
+        const scale = $gameScreen.zoomScale();
+        this.move(sprite.x * scale - this.width, sprite.y * scale, 300, 300);
         this.selectLast();
         this.showHelpWindow();
         super.show();
